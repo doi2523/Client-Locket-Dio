d56b1563378c9f0501ace9370b7c7d6001c54983
dio <doibncm2003@gmail.com>
2025-08-27 04:59:51 +0700

    remove no use

--- src/components/Background/BgCustome/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-const BgCustome = () => {
-  return <div className="bg-custome"></div>;
-};
-
-export default BgCustome;
--- src/components/Background/BgLocket/index.jsx
+++
@@ -1,8 +0,0 @@
-const BgLocket = () => {
-    return (
-      <div className="fixed inset-0 bg-locket -z-50"></div>
-    );
-  };
-  
-  export default BgLocket;
-  
