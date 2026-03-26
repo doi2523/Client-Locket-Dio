2ae11d4b0ba3aac47e1486509e5b3b23b6e8dd98
dio <doibncm2003@gmail.com>
2026-03-26 22:20:55 +0700

    refactor: clean and rm api routes list and use function services

--- apps/main/src/utils/index.js
+++ apps/main/src/utils/index.js
@@ -1,6 +1,5 @@
 export * from "./storage/helpers"; // Xuất tất cả hàm từ helpers.js
 export * from "./process/PrsImage/createImage"; // Xuất tất cả hàm từ cropimages.js
-export * from "./API/apiRoutes"; // Xuất tất cả hàm từ apiRoutes.js
 export * from "./storage";
 export * from "./standardize";
 export * from "./auth";
