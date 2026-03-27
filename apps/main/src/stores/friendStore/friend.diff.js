4a43ede8c8225bf673ae0364b9bdc7226c5b8e27
dio <doibncm2003@gmail.com>
2026-03-27 11:51:26 +0700

    chore: clean and rm friendstore v2

--- apps/main/src/stores/friendStore/friend.diff.js
+++
@@ -1,9 +0,0 @@
-export const diffFriendIds = (apiFriends, cachedIds) => {
-  const apiUidSet = new Set(apiFriends.map((f) => f.uid));
-  const cachedUidSet = new Set(cachedIds.map((f) => f.uid));
-
-  const newIds = apiFriends.filter((f) => !cachedUidSet.has(f.uid));
-  const removedIds = cachedIds.filter((f) => !apiUidSet.has(f.uid));
-
-  return { newIds, removedIds };
-};
