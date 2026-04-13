a047ba685c5b28f9c197d54f59d33afde386435c
dio <doibncm2003@gmail.com>
2026-04-13 22:51:36 +0700

    fix: update state store and optimize

--- apps/main/src/features/EditorCaption/index.jsx
+++ apps/main/src/features/EditorCaption/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./styles.css";
 import { PiClockFill } from "react-icons/pi";
 import SnowEffect from "@/components/Effects/SnowEffect";
 import ReviewOverlay from "./components/ReviewOverlay";
@@ -8,6 +7,7 @@ import DecorativeOverlay from "./components/DecorativeOverlay";
 import { getCaptionStyle } from "@/helpers/styleHelpers";
 import IconRenderer from "../OverlayRender/iconRenders";
 import StreakOverlay from "./components/StreakOverlay";
+import MusicOverlay from "./components/MusicOverlay";
 
 // Custom Hooks
 const useTextMeasurement = (text, ref, type, placeholder, parentRef) => {
@@ -129,48 +129,6 @@ const CaptionIconOverlay = ({
   );
 };
 
-const MusicOverlay = ({ postOverlay }) => {
-  const music = postOverlay?.payload || {};
-
-  return (
-    <div className="flex w-auto items-center gap-2 py-2 px-4 rounded-4xl text-white font-semibold bg-white/50 backdrop-blur-2xl max-w-[85%] overflow-hidden">
-      <img
-        src={music.image}
-        alt="Cover"
-        className="w-6 h-6 object-cover rounded-sm shrink-0 no-select no-save"
-      />
-
-      <div className="relative overflow-hidden whitespace-nowrap flex-1">
-        <div
-          className="inline-block animate-marquee"
-          style={{
-            animationDuration:
-              postOverlay.text.length < 30
-                ? "9s"
-                : postOverlay.text.length < 60
-                  ? "15s"
-                  : "17s",
-          }}
-        >
-          <span className="mr-4">{postOverlay.text}</span>
-          <span className="mr-4 absolute">{postOverlay.text}</span>
-        </div>
-      </div>
-      {/* ✅ Chỉ hiện icon nếu platform là spotify */}
-      {music.platform === "spotify" && (
-        <div className="flex items-center gap-2 shrink-0 no-select no-save">
-          <div className="border-l border-white h-5"></div>
-          <img
-            src="./icons/spotify_icon.png"
-            alt="Spotify Icon"
-            className="w-6 h-6 object-contain"
-          />
-        </div>
-      )}
-    </div>
-  );
-};
-
 const WeatherOverlay = ({ postOverlay }) => {
   return (
     <div className="flex items-center bg-white/50 backdrop-blur-2xl gap-1 py-2 px-4 rounded-4xl text-white font-semibold">
@@ -223,10 +181,11 @@ const LocationOverlay = ({
         ref={textareaRef}
         value={postOverlay.caption || ""}
         onChange={(e) =>
-          setPostOverlay((prev) => ({
-            ...prev,
+          setPostOverlay({
+            ...postOverlay,
+            text: e.target.value,
             caption: e.target.value,
-          }))
+          })
         }
         placeholder={placeholder}
         rows={1}
