e008dab85688c03a00c95799eaf351d5db495e63
dio <doibncm2003@gmail.com>
2026-05-23 16:08:51 +0700

    refactor: add more icons, fix rendering logic

--- apps/main/src/components/Overlay/icons/SFSymbolIcon.jsx
+++ apps/main/src/components/Overlay/icons/SFSymbolIcon.jsx
@@ -1,12 +1,28 @@
-const getSFSymbolUrl = (name, color = "#000") => {
-  const encodedColor = encodeURIComponent(color);
-  return `https://sfsymbols.vercel.app/s/${name}?color=${encodedColor}`;
-};
+const icons = import.meta.glob("/src/assets/sf-symbols/*.{png,svg}", {
+  eager: true,
+  import: "default",
+});
 
-const SFSymbolIcon = ({ data, color }) => {
-  const url = getSFSymbolUrl(data, color);
+const SFSymbolIcon = ({ data, className = "w-6 h-6 object-cover" }) => {
+  const src =
+    icons[`/src/assets/sf-symbols/${data}.svg`] ||
+    icons[`/src/assets/sf-symbols/${data}.png`];
 
-  return <img src={url} alt={data} className="w-6 h-6 object-cover" />;
+  if (!src) {
+    console.warn(`Symbol "${data}" not found.`);
+
+    return null;
+  }
+
+  return (
+    <img
+      src={src}
+      alt={data}
+      className={className}
+      draggable={false}
+      loading="lazy"
+    />
+  );
 };
 
 export default SFSymbolIcon;
